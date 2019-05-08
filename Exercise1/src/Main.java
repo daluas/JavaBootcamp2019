d04f28a1db3e3c89762889b90261f680c608734f
stefancernescu <stefan.cernescu94@gmail.com>
2019-05-08 09:32:00 +0300

    Added first exercise

---
+++ Exercise1/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //TODO NumberWithSquareSumOfSquareDivisorsFinder
+        /// Given the limits of an interval, find all numbers in that interval for which the sum of the square of it's divisors is a perfect square
+        /// The interval is closed on both ends (the limits should also be checked)
+        /// Example:
+        /// The divisors of 42 are [1, 2, 3, 6, 7, 14, 21, 42]
+        /// The divisors squared are [1, 4, 9, 36, 49, 196, 441, 1764]
+        /// The sum of the squared divisors is 2500 which is the perfect square of 50
+        /// This means 42 respects the rule
+
+        List<Student> students = new ArrayList<>();
+
+        Student student1 = new Student(22, "Ionel", "Popescu");
+        Student student2 = new Student(18, "Mihai", "Ionescu");
+        Student student3 = new Student(21, "Ghita", "Muresan");
+        Student student4 = new Student(17, "Dorel", "Constructor");
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+
+        //TODO Sort the students by age
+        //TODO Display only the students that are above 20 years
+        //TODO Display only the students that have their lastname ending in escu
+        //TODO Create a new Class named Grades with the following members: score and disciplineName. Each Student can have one or more grades at a certain discipline
+        //TODO Add grades to the students
+        //TODO Create a method that displays the the lastName of the students that have no grade lower than 5.
+
+
+    }
+}
