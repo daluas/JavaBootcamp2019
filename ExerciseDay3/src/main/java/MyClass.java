68e7446607e8ff38dd84c3bf37e012f8e125ccc3
bootcamp-PC15\bootcamp <marilena.dascalu@cegeka.com>
2019-05-10 12:33:09 +0300

    Am adaugat atat codare cat si decodare la exercitiul 2

--- ExerciseDay3/src/main/java/MyClass.java
+++ ExerciseDay3/src/main/java/MyClass.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
-
+import java.util.function.IntUnaryOperator;
 public class MyClass {
     //Rezolvare Exericitiu 1 din Java8Exercises
     public static String verifyOddEven(int number) {
@@ -25,7 +25,7 @@ public class MyClass {
 
     //Rezolvare Exercitiu 2 din Java8Exercises
     public static final int N = 5;
-    public static char findNextLetter(int letter)
+    public static int findNextLetter(int letter)
     {
         boolean isUpper = false;
         if(letter == 32)
@@ -34,29 +34,51 @@ public class MyClass {
         }
         else
         {
-            if(letter<=90)
+            if(letter <= 90)
             {
-                letter+=32;
-                isUpper=true;
+                letter += 32;
+                isUpper = true;
             }
             if(letter+N > 122)
             {
-               return (isUpper)? (char)(letter = N - 58 + letter) : (char)(letter = N - 26 + letter) ;
+               return (isUpper)? (letter = N - 58 + letter) : (letter = N - 26 + letter) ;
             }
-             return (isUpper)?(char)(letter= letter - (32 - N)):(char)(letter += N);
+             return (isUpper)?(letter = letter - (32 - N)):(letter += N);
 
         }
     }
-    public static Optional<String> decodeCesar(String codeString)
+    public static int findPreviousLetter(int letter)
     {
-        if(codeString == null || codeString.equals("") || !codeString.matches("[A-Za-z ]*"))
+        boolean isUpper = false;
+        if(letter == (32 + N))
+        {
+            return (letter -= N);
+        }
+        else
+        {
+            if(letter < 97)
+            {
+                letter += 32;
+                isUpper = true;
+            }
+            if(letter-N < 97)
+            {
+                return (isUpper)? (letter = 123 - N + (letter-97) - 32) : (letter = 123 - N + (letter-97)) ;
+            }
+            return (isUpper)?(letter = letter - (32 + N)):(letter -= N);
+
+        }
+    }
+    public static Optional<String> codeOrDecodeCesar(String codeString,IntUnaryOperator modify)
+    {
+        if(codeString == null || codeString.equals("") || !codeString.matches("[A-Za-z% ]*"))
         {
             return Optional.empty();
         }
         char[] arrayOfCode = codeString.toCharArray();
         for(int i = 0 ;i<arrayOfCode.length;i++)
         {
-            arrayOfCode[i] = findNextLetter(arrayOfCode[i]);
+            arrayOfCode[i] = (char)modify.applyAsInt(arrayOfCode[i]);
         }
         return Optional.of(new String(arrayOfCode));
     }
@@ -70,10 +92,15 @@ public class MyClass {
 
         //TODO Java8Exercises Exercise 2
         System.out.println("====Exercise2====");
-        System.out.println(decodeCesar("Ana are mere zero Zero WORA").orElse("String null or empty or not respecting format"));
-        System.out.println(decodeCesar("").orElse("String null or empty or not respecting format"));
-        System.out.println(decodeCesar(null).orElse("String null or empty or not respecting format"));
-        System.out.println(decodeCesar("qwdqwdqwd4342423424").orElse("String null or empty or not respecting format"));
+        System.out.println("Codarea datelor");
+        String firstCode = codeOrDecodeCesar("Ana are mere zero Zero WORA",MyClass::findNextLetter).orElse("String null or empty or not respecting format");
+        System.out.println(firstCode);
+        System.out.println(codeOrDecodeCesar("",MyClass::findNextLetter).orElse("String null or empty or not respecting format"));
+        System.out.println(codeOrDecodeCesar(null,MyClass::findNextLetter).orElse("String null or empty or not respecting format"));
+        System.out.println(codeOrDecodeCesar("qwdqwdqwd4342423424",MyClass::findNextLetter).orElse("String null or empty or not respecting format"));
+        System.out.println("Decodarea datelor");
+        System.out.println(codeOrDecodeCesar(firstCode,MyClass::findPreviousLetter).orElse("String null or empty or not respecting format"));
+
 
         //TODO Java8Exercises Exercise 3
         System.out.println("====Exercise3====");
