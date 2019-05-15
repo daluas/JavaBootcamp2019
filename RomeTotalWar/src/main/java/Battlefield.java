f98c7f9b689fb5fe211be1379b2dddb4151e0749
bootcamp-PC14\bootcamp <elena.magdalena@cegeka.com>
2019-05-15 15:22:31 +0300

    Fixed typos

--- RomeTotalWar/src/main/java/Battlefield.java
+++ RomeTotalWar/src/main/java/Battlefield.java
@@ -14,16 +14,16 @@ public class Battlefield {
         Unit BrutiiArmy = new Army("Brutii");
         Unit BrutiiCavalry = new Cavalry();
         Unit BrutiiSkirmishers = new Skirmisher();
-        BrutiiArmy.add((BrutiiCavalry));
-        BrutiiArmy.add((BrutiiSkirmishers));
+        BrutiiArmy.add(BrutiiCavalry);
+        BrutiiArmy.add(BrutiiSkirmishers);
         Unit RomanArmy = Army.JoinForces(JuliiArmy,BrutiiArmy, "Roman");
 
 
         Unit GaulFrontArchers = new Archer();
         Unit GaulRightArchers = new Archer();
         Unit GaulPikemen = new Pikemen();
-        Unit GaulSkirmishCavaltry = new Cavalry();
-        Unit GaulBarnarianCavalry = new Cavalry();
+        Unit GaulSkirmishCavalry = new Cavalry();
+        Unit GaulBarbarianCavalry = new Cavalry();
         Unit GaulSkirmishers = new Skirmisher();
         Unit GaulAxemen = new HeavyInfantry();
         Unit GaulArmy = new Army("Gaul");
@@ -32,15 +32,10 @@ public class Battlefield {
         GaulArmy.add(GaulFrontArchers);
         GaulArmy.add(GaulRightArchers);
         GaulArmy.add(GaulPikemen);
-        GaulArmy.add(GaulSkirmishCavaltry);
-        GaulArmy.add(GaulBarnarianCavalry);
+        GaulArmy.add(GaulSkirmishCavalry);
+        GaulArmy.add(GaulBarbarianCavalry);
         GaulArmy.add(GaulSkirmishers);
         GaulArmy.add(GaulAxemen);
-
-        System.out.println(GaulArmy.getDefensePower());
-        System.out.println(GaulArmy.getOffensePower());
-        System.out.println(RomanArmy.getOffensePower());
-        System.out.println(RomanArmy.getDefensePower());
 
         Subject battleOfAlesia = new Battle("Battle of Alesia");
         Observer SPQR = new CommandCenter();
